185de48fa11bebe149103b3ee8154a13b9be7913
the-Jamz <james@jamz.dev>
2020-06-20 14:29:31 +0100

    Fix clear

--- src/output.rs
+++ src/output.rs
@@ -64,7 +64,7 @@ fn parse(s: &str) {
 }
 
 pub fn clear() {
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    println!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 }
 
 pub struct Preferences {
